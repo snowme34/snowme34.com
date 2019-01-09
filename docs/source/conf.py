917d8e1e8f3de2fd031920f21c325fe05605d7c2
snowme34 <snowme34@gmail.com>
2019-01-08 21:07:11 -0800

    sphinx markdown table experiment with nihongo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.githubpages',
+    'sphinx-markdown-tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
