da9896a8091710d4c026c50ede4125e47a6b0e17
snowme34 <snowme34@users.noreply.github.com>
2019-12-31 02:02:03 -0800

    travis ci back to production

--- hexo-build-if-updated.sh
+++ hexo-build-if-updated.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# if [[ -n "$(git diff --name-status HEAD~1...HEAD blog)" ]]; then
-cp blog/themes/_config.yml blog/themes/symphony/
-npx hexo --cwd blog generate
-# fi
+if [[ -n "$(git diff --name-status HEAD~1...HEAD blog)" ]]; then
+  cp blog/themes/_config.yml blog/themes/symphony/
+  npx hexo --cwd blog generate
+fi
