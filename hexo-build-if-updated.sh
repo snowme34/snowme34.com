53498200815b9c5619850697dfa2022de48df2a1
snowme34 <snowme34@users.noreply.github.com>
2019-12-31 01:56:02 -0800

    update hexo logic

--- hexo-build-if-updated.sh
+++ hexo-build-if-updated.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+set -e
+
 # if [[ -n "$(git diff --name-status HEAD~1...HEAD blog)" ]]; then
-cd blog
-npm install
-npx hexo generate
+cp blog/themes/_config.yml blog/themes/symphony/
+npx hexo --cwd blog generate
 # fi
